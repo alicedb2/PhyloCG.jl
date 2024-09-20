a0b6e6886c4c331032ca45e03b64088aa9ceca05
Alice Doucet Beaupré <alice.doucet.beaupre@gmail.com>
2024-09-20 14:17:29 -0400

    added H model

--- src/simulation.jl
+++ src/simulation.jl
@@ -3,20 +3,20 @@ mutable struct Pop
     k::Int64
 end
 
-function advance_gillespie_bdi(tk::Pop, b, d, rho, g; max_age=1.0, max_size=10000)
+function advance_gillespie_bdih(tk::Pop, b, d, rho, g, eta, alpha, beta; max_age=1.0, max_size=10000)
     t, k = tk.t, tk.k
     if k == 0 || t >= max_age
         return Pop(max_age, k)
     end
 
-    rates = [b * k, d * k, rho * k]
+    rates = [b * k, d * k, rho * k, eta * k]
     total_rate = sum(rates)
     r1 = rand()
     delta_age = -log(r1)/total_rate
     if t + delta_age >= max_age
         return Pop(max_age, k)
     end
-    event = sample([1, 2, 3], Weights(rates))
+    event = sample([1, 2, 3, 4], Weights(rates))
     if event == 1
         return Pop(t + delta_age, k + 1)
     elseif event == 2
@@ -24,6 +24,9 @@ function advance_gillespie_bdi(tk::Pop, b, d, rho, g; max_age=1.0, max_size=1000
     elseif event == 3
         delta_k = rand(Geometric(1 - g))
         return Pop(t + delta_age, k + delta_k)
+    elseif event == 4
+        _g = rand(Beta(alpha, beta))
+        delta_k = rand(Geometric(1 - _g))
+        return Pop(t + delta_age, k + delta_k)
     end
 end
-
